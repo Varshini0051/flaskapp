e53831845a38647c4af7f205305941bfcebe9e3d
Varshini0051 <Varshini.Sivakumar@bigthinkcode.com>
2023-04-12 15:01:30 +0530

    token authorization

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, redirect, session, jsonify,g
+import re
 import psycopg2
+from functools import wraps
+import jwt
 import hashlib
+from datetime import timedelta
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 
 app = Flask(__name__)
 app.secret_key = "mysecretkey"
+app.config['JWT_SECRET_KEY'] = 'c226cf6f52b5df05f0e8e892fadb67f8af55e4ec446f5bb7b0ac197257dbf882' 
+jwt = JWTManager(app)
+
 
 DATABASE = {
     'host': 'localhost',
@@ -29,35 +37,71 @@ def close_db(error):
         db.close()
 
 @app.route("/")
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.headers.get('Authorization')
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            g.user_id = data['id']
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+        return f(*args, **kwargs)
+    return decorated
 
 
-@app.route("/register", methods=["GET","POST"])
+@app.route("/register", methods=["POST"])
 def register():
-    if request.method == "POST":
-        get_data=request.get_json()
-        username = get_data["username"]
-        email = get_data["email"]
-        password = get_data["password"]
-        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    data = request.get_json()
+    username = data["username"]
+    email = data["email"]
+    password = data["password"]
 
-        db = get_db()
-        cur = db.cursor()
-        cur.execute(
-            "INSERT INTO users (username, email, password) VALUES (%s, %s, %s)",
-            (username, email, hashed_password),
-        )
-        db.commit()
+    # Username validation
+    if not re.match(r'^[a-zA-Z0-9]{5,20}$', username):
+        return jsonify({"success": False, "message": "Username is invalid"}), 401
+
+    # Email validation
+    if not re.match(r'^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email):
+        return jsonify({"success": False, "message": "Email is invalid"}), 401
+
+    # Password validation
+    if not re.match(r'^.{5,}$', password):
+        return jsonify({"success": False, "message": "Password is invalid"}), 401
+
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+
+    db = get_db()
+    cur = db.cursor()
+
+    # Check if username or email already exists in database
+    cur.execute(
+        "SELECT * FROM users WHERE username=%s OR email=%s",
+        (username, email)
+    )
+    user = cur.fetchone()
+
+    if user is not None:
         cur.close()
         db.close()
+        return jsonify({"success": False, "message": "Username or email already exists"})
 
-        if get_data is not None:
-            return jsonify({"success": True, "message": "Registration successful"})
-        else:
-            return jsonify({"success": False, "message": "Invalid email or password"})
-    else:
-        return jsonify({"message": "Method not allowed"}), 405
+    # Insert data into database
+    cur.execute(
+        "INSERT INTO users (username, email, password) VALUES (%s, %s, %s)",
+        (username, email, hashed_password)
+    )
+    db.commit()
 
-@app.route("/login", methods=["GET","POST"])
+    cur.close()
+    db.close()
+
+    return jsonify({"success": True, "message": "Registration successful"})
+
+
+@app.route("/login", methods=["POST"])
 def login():
     data = request.get_json()
     email = data["email"]
@@ -75,10 +119,12 @@ def login():
     db.close()
 
     if user:
-        session["user_id"] = user[0]
-        return jsonify({"success": True, "message": "Login successful"})
+        # Generate and return JWT access token
+        access_token = create_access_token(identity=user[0], expires_delta=timedelta(hours=1))
+        return jsonify({"success": True, "access_token": access_token})
     else:
-        return jsonify({"success": False, "message": "Invalid email or password"})
+        return jsonify({"success": False, "message": "Invalid email or password"}), 401
+
 
     # return jsonify("hi")
 
@@ -122,13 +168,44 @@ def get_users():
 
 
 
-@app.route('/users/<int:user_id>', methods=["GET", "POST","PATCH"])
-def get_user(user_id):
+# @app.route('/users/<int:user_id>', methods=["GET", "POST","PATCH"])
+# def get_user(user_id):
+#     db = get_db()
+#     cur = db.cursor()
+
+#     cur.execute('SELECT * FROM users WHERE id = %s', (user_id,))
+
+#     row = cur.fetchone()
+
+#     cur.close()
+#     db.close()
+
+#     if row is not None:
+#         user = {"id": row[0], "username": row[1], "email": row[2]}
+#         return jsonify(user)
+#     else:
+#         return jsonify({"message": "User not found"}), 404
+
+@app.route('/users/<int:user_id>', methods=['PATCH'])
+@token_required
+def update_user(user_id):
+    if g.user_id != user_id:
+        return jsonify({'message': 'You are not authorized to modify this user!'}), 401
+
+    # connect to the database
     db = get_db()
     cur = db.cursor()
 
-    cur.execute('SELECT * FROM users WHERE id = %s', (user_id,))
+    # get the request data
+    data = request.json
+    username = data.get("username")
+    email = data.get("email")
+    # update the user details in the database
+    cur.execute('UPDATE users SET username=%s, email=%s WHERE id=%s', (username, email, user_id))
+    db.commit()
 
+    # fetch the updated user details from the database
+    cur.execute('SELECT * FROM users WHERE id = %s', (user_id,))
     row = cur.fetchone()
 
     cur.close()
